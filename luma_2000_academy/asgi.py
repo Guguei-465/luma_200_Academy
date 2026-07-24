98a1a2d0c6ba6c9a224bd004e83cab5b790d0e0f
Guguei-465 <riakgugwey@gmail.ccom>
2026-07-24 09:19:40 +0300

    uptdate

--- luma_2000_academy/asgi.py
+++ luma_2000_academy/asgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 import sys
 from django.core.asgi import get_asgi_application
-sys.path.append("/home/ryackson/www/ryacksonfungo_luma")
+sys.path.append("/home/ryacksonfungo/www/ryacksonfungo_luma")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'luma_2000_academy.settings')
 
 application = get_asgi_application()
