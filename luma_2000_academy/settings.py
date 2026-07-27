46c65a5f02bc2f484fb0fd95003fa0c5c20d5f4f
Guguei-465 <riakgugwey@gmail.ccom>
2026-07-27 09:09:09 +0300

    one

--- luma_2000_academy/settings.py
+++ luma_2000_academy/settings.py
@@ -82,9 +82,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_CREDENTIALS = True
+
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1",
-    "http://localhost",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
     "https://ryacksonfungo.alwaysdata.net",
 ]
 
