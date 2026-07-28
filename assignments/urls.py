05c56cb1aca56d45356d26f15d38638bc4b73223
Guguei-465 <riakgugwey@gmail.ccom>
2026-07-28 22:33:34 +0300

    uu

--- assignments/urls.py
+++ assignments/urls.py
@@ -1,18 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 from .views import (
     TeacherAssignmentListView,
     TeacherAssignmentDetailView,
     TeacherAssignmentCreateView,
     TeacherAssignmentUpdateView,
     TeacherAssignmentDeleteView,
+    TeacherProfileViewSet,
 )
-from rest_framework.routers import DefaultRouter
-from .views import TeacherProfileViewSet
 
 router = DefaultRouter()
-router.register("profiles", TeacherProfileViewSet, basename="teacher-profile")
-
-urlpatterns = router.urls
+router.register(r"teacher-profile", TeacherProfileViewSet, basename="teacher-profile")
 
 urlpatterns = [
     path("", TeacherAssignmentListView.as_view(), name="assignment-list"),
@@ -20,4 +19,7 @@ urlpatterns = [
     path("create/", TeacherAssignmentCreateView.as_view(), name="assignment-create"),
     path("update/<int:pk>/", TeacherAssignmentUpdateView.as_view(), name="assignment-update"),
     path("delete/<int:pk>/", TeacherAssignmentDeleteView.as_view(), name="assignment-delete"),
+
+    # Include the router URLs
+    path("", include(router.urls)),
 ]
