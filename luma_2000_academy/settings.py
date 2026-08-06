fa37f155492ed60c6e331da4e381d158c6aa39e5
Guguei-465 <riakgugwey@gmail.ccom>
2026-08-06 21:21:48 +0300

    i9o

--- luma_2000_academy/settings.py
+++ luma_2000_academy/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_rest_passwordreset'
+    'django_rest_passwordreset',
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
