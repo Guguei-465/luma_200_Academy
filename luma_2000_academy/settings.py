15336f206b5e6fa74b9d0088b7591925a63cc45d
Guguei-465 <riakgugwey@gmail.ccom>
2026-08-17 21:21:19 +0300

    aloo

--- luma_2000_academy/settings.py
+++ luma_2000_academy/settings.py
@@ -101,6 +101,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://school-management-system-chi-eight.vercel.app/",
     "https://ryacksonfungo.alwaysdata.net",
 ]
 
